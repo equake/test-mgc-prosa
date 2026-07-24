3b894db79d69817a58a3d080430779cd7e5fa00b
GitHub Action <action@github.com>
2026-07-24 19:01:20 +0000

    Bump version: 0.1.2 → 0.1.3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.routes.cnpj import router as cnpj_router
 from app.routes.cpf import router as cpf_router
 
-app = FastAPI(title="API Validação CNPJ e CPF", version="0.1.2")
+app = FastAPI(title="API Validação CNPJ e CPF", version="0.1.3")
 
 app.include_router(cnpj_router)
 app.include_router(cpf_router)
